df3004e5e0edd7e3c3b50a174dd9d77b5348b811
riku546 <riku62560@gmail.com>
2025-07-23 00:36:03 +0900

    ミスを修正

--- src/app/api/event/feedback/frourio.ts
+++ src/app/api/event/feedback/frourio.ts
@@ -1,10 +1,10 @@
 import type { FrourioSpec } from '@frourio/next';
 import { z } from 'zod';
-import { feedbackSchema } from '../../(type)/event';
+import { feedbackSchemaAtUpdate } from '../../(type)/event';
 
 export const frourioSpec = {
   patch: {
-    body: z.object({ eventId: z.string(), feedback: feedbackSchema }),
+    body: z.object({ eventId: z.string(), feedback: feedbackSchemaAtUpdate }),
     res: {
       204: { body: z.object({ message: z.string() }) },
       500: { body: z.object({ message: z.string() }) },
