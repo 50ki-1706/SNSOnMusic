7296813e57308a4d241bb70617700f6cef0f5ae7
riku546 <riku62560@gmail.com>
2025-07-26 12:25:22 +0900

    blog commentとそれのいいねapiを作成

---
+++ src/app/api/blog/[id]/comment/route.ts
@@ -0,0 +1,20 @@
+import { userIdInApi } from '@/app/api/(lib)/userIdInApi';
+import { createComment } from '@/app/api/(Repository)/blog';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const POST = async (req: NextRequest, { params }: { params: { id: string } }) => {
+  try {
+    const blogId = params.id;
+    const userId = await userIdInApi();
+
+    // リクエストボディからcontentを取得
+    const { content } = await req.json();
+
+    await createComment(blogId, userId, content);
+
+    // 204 No Content を返す
+    return NextResponse.json(null, { status: 204 });
+  } catch (error) {
+    return NextResponse.json({ message: 'failed to post comment' }, { status: 500 });
+  }
+};
